645d7d22807d610314893df004c433fa6cadc923
João Neto <joaoneto.20fsa@gmail.com>
2020-06-17 18:12:29 -0300

    Single player feito
    Agora falta a seção de testes

--- js/single_player.js
+++ js/single_player.js
@@ -4,11 +4,87 @@
     const btn_soma_3 = document.querySelector("#soma_3");
     const placar = document.querySelector(".placar");
     const status = document.querySelector(".status");
+    let lastPlay;
 
     // SETANDO VALORES INICIAIS
     totalScore = 0;
     placar.innerHTML = totalScore;
     status.innerHTML = "Sua vez";
+
+    // JOGADOR
+    function addScorePlayer(scorePlayer){
+        if(totalScore < 21){
+            totalScore += scorePlayer;
+            placar.innerHTML = totalScore;
+            lastPlay = "Jogador";
+            console.log(lastPlay);
+        }
+    }
+
+    // COMPUTADOR
+    function addScoreComputer(){
+        // DESATIVAR BOTÕES DO USUÁRIO
+        btn_soma_1.setAttribute("disabled", true);
+        btn_soma_2.setAttribute("disabled", true);
+        btn_soma_3.setAttribute("disabled", true);
+
+        totalScore < 21 ? status.innerHTML = "Vez do computador": ""
+        // DELAY PARA O PC JOGAR
+        setTimeout(() => {
+            if(totalScore < 21){
+                const scoreComputer =  getRandomInt(1, 4);
+                totalScore += scoreComputer;
+                placar.innerHTML = totalScore;
+                /*
+                    SE O SCORE FOR MENOR QUE 21, QUANDO O COMPUTADOR TERMINAR A JOGADA, DEVOLVE A VEZ PARA O PLAYER
+                */
+                totalScore < 21 ? status.innerHTML = "Sua vez": ""
+                // REATIVA BOTÕES DO USUÁRIO
+                btn_soma_1.removeAttribute("disabled");
+                btn_soma_2.removeAttribute("disabled");
+                btn_soma_3.removeAttribute("disabled");
+                lastPlay = "Computador";
+                console.log(lastPlay);
+            }
+        }, 2000)
+    }
+
+    // VERIFICAÇÃO
+    function verifyScore(score){
+        console.log("Verificou");
+        if(score >= 21){
+            status.innerHTML = "Fim de jogo";
+
+            if(lastPlay == "Jogador"){
+                Swal.fire({
+                    icon: "success",
+                    title: "Fim de jogo",
+                    text: `Voce venceu!!`
+                })
+            }
+            else{
+                Swal.fire({
+                    icon: "error",
+                    title: "Fim de jogo",
+                    text: `Voce perdeu`
+                })
+            }
+            
+            setTimeout(() => {
+                // SETANDO VALORES INICIAIS
+                totalScore = 0;
+                placar.innerHTML = totalScore;
+                status.innerHTML = "Sua vez";
+            }, 2000)
+        }
+    }
+
+    // GERAR NÚMEROS INTEIROS ALTEATÓRIOS
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
     btn_soma_1.addEventListener("click", () => {
         addScorePlayer(1);
@@ -31,56 +107,7 @@
         addScoreComputer();
         setTimeout(() => {
             verifyScore(totalScore);
-        }, 3000)
+        }, 2500)
     })
-
-    // JOGADOR
-    function addScorePlayer(scorePlayer){
-        if(totalScore < 21){
-            totalScore += scorePlayer;
-            placar.innerHTML = totalScore;
-            console.log("Player jogou");
-        }
-    }
-
-    // COMPUTADOR
-    function addScoreComputer(){
-        status.innerHTML = "Vez de computador";
-        // DELAY PARA O PC JOGAR
-        setTimeout(() => {
-            if(totalScore < 21){
-                const scoreComputer =  getRandomInt(1, 4);
-                totalScore += scoreComputer;
-                placar.innerHTML = totalScore;
-                /*
-                    QUANDO O COMPUTADOR TERMINAR A JOGADA, DEVOLVE A VEZ PARA O PLAYER
-                */
-                status.innerHTML = "Sua vez";
-                console.log("Computador jogou");
-            }
-        }, 2000)
-    }
-
-    // VERIFICAÇÃO
-    function verifyScore(score){
-        console.log("Verificou");
-        if(score >= 21){
-            alert("Fim de jogo");
-
-            setTimeout(() => {
-                // SETANDO VALORES INICIAIS
-                totalScore = 0;
-                placar.innerHTML = totalScore;
-                status.innerHTML = "Sua vez";
-            }, 3000)
-        }
-    }
-
-    // GERAR NÚMEROS INTEIROS ALTEATÓRIOS
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
 
 })();
