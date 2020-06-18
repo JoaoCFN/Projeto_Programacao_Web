41acbf604457473025cfa1720b86777d1de5b71c
João Neto <joaoneto.20fsa@gmail.com>
2020-06-18 16:21:56 -0300

    Corigindo lógica de desabilitar botões no multi

--- js/multi_player.js
+++ js/multi_player.js
@@ -32,8 +32,8 @@
             totalScore = Number(button.innerHTML);
             placar.innerHTML = totalScore;
             lastPlay = "Jogador 1";
-            updateButtons();
             updateStatus();
+            updateButtons();
         }
     }
 
@@ -42,38 +42,10 @@
             totalScore = Number(button.innerHTML);
             placar.innerHTML = totalScore;
             lastPlay = "Jogador 2";
-            updateButtons();
             updateStatus();
+            updateButtons();
         }
     }
-
-    // COMPUTADOR
-    function addScoreComputer(){
-        // // DESATIVAR BOTÕES DO USUÁRIO
-        // btn_soma_1.setAttribute("disabled", true);
-        // btn_soma_2.setAttribute("disabled", true);
-        // btn_soma_3.setAttribute("disabled", true);
-
-        // // ALTERANDO STATUS
-        // totalScore < 21 ? status.innerHTML = "Vez do computador": ""
-
-        // // DELAY PARA O PC JOGAR
-        // setTimeout(() => {
-        //     if(totalScore < 21){
-        //         const scoreComputer =  getRandomInt(1, 4);
-        //         totalScore += scoreComputer;
-        //         placar.innerHTML = totalScore;
-        //         /*
-        //             SE O SCORE FOR MENOR QUE 21, QUANDO O COMPUTADOR TERMINAR A JOGADA, DEVOLVE A VEZ PARA O PLAYER
-        //         */
-        //         totalScore < 21 ? status.innerHTML = "Sua vez": ""
-        //         lastPlay = "Computador";
-        //         updateButtons();
-        //     }
-        // }, 2000)
-    }
-
-
 
     // ATUALIZA OS VALORES DOS BOTÕES
     function updateButtons(){
@@ -82,32 +54,47 @@
         btn_soma_2.innerHTML = totalScore + 2;
         btn_soma_3.innerHTML = totalScore + 3;
         // PLAYER 2
-        /*
-            OS BOTÕES DO PLAYER 2 RECEBEM OS DADOS DO BOTÃO DO PLAYER 1
-        */
-        btn_soma_4.innerHTML = btn_soma_1.innerHTML;
-        btn_soma_5.innerHTML = btn_soma_2.innerHTML;
-        btn_soma_6.innerHTML = btn_soma_3.innerHTML;
+        btn_soma_4.innerHTML = totalScore + 1;
+        btn_soma_5.innerHTML = totalScore + 2;
+        btn_soma_6.innerHTML = totalScore + 3;
 
-        // btn_soma_1.removeAttribute("disabled");
-        // btn_soma_2.removeAttribute("disabled");
-        // btn_soma_3.removeAttribute("disabled");
+        if(player1.status == "Sua vez"){
+            btn_soma_1.removeAttribute("disabled");
+            btn_soma_2.removeAttribute("disabled");
+            btn_soma_3.removeAttribute("disabled");
 
-        // btn_soma_4.removeAttribute("disabled");
-        // btn_soma_5.removeAttribute("disabled");
-        // btn_soma_6.removeAttribute("disabled");
+            btn_soma_4.setAttribute("disabled", true);
+            btn_soma_5.setAttribute("disabled", true);
+            btn_soma_6.setAttribute("disabled", true);
+
+        }
+        else if(player2.status == "Sua vez"){
+            btn_soma_4.removeAttribute("disabled");
+            btn_soma_5.removeAttribute("disabled");
+            btn_soma_6.removeAttribute("disabled");
+
+            btn_soma_1.setAttribute("disabled", true);
+            btn_soma_2.setAttribute("disabled", true);
+            btn_soma_3.setAttribute("disabled", true);
+        }
     }
 
     function updateStatus(){
         if(player1.status == "Sua vez"){
+            // PLAYER 1
             player1.status = "Vez do oponente";
             status_player1.innerHTML = player1.status;
+
+            // PLAYER 2
             player2.status = "Sua vez";
             status_player2.innerHTML = player2.status;
         }
         else{
+            // PLAYER 2
             player2.status = "Vez do oponente";
             status_player2.innerHTML = player2.status;
+
+            // PLAYER 1
             player1.status = "Sua vez";
             status_player1.innerHTML = player1.status;
         }
